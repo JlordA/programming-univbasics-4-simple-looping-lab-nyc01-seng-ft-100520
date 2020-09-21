d562d02dc490b73fbf2ff59868097680a573c332
placid-tier-4550 <james.ardery@gmail.com>
2020-09-21 15:50:55 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,24 @@
 # Write your methods here
+def loop_message_five_times(message)
+  counter = 0
+  while counter <= 5
+  puts message
+  counter += 1
+ end
+end
+
+def loop_message_n_times(message, number)
+  counter = 0
+  while counter <= number
+    puts message
+    counter += 1
+  end
+end
+
+def output_array(array)
+  counter = 0
+  while array[counter] do
+    puts array[counter]
+    counter =+ 1
+  end
+end
