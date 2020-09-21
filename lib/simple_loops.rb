88f8577ffe79839c5fc15e0db5bad075a462232e
placid-tier-4550 <james.ardery@gmail.com>
2020-09-21 16:20:22 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,10 +25,10 @@ end
 
 def return_string_array(array)
   counter = 0
+  new_array = []
   while counter < array.length do
-    array = Array.new
-    array[counter].to_s << array
+    new_array.push([counter].to_s)
     counter += 1
   end
-  array
+  new_array
 end
